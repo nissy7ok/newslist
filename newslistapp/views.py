3ced409c610da8e5806cdd5af85ad7efe6d86fd4
nissy7ok <nishimrua.n.ku@gmail.com>
2020-08-16 17:08:26 +0900

    create 'base.html'
    create 'base.html' with bootstrap

--- newslistapp/views.py
+++ newslistapp/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Indexです！")
+    # return HttpResponse("Indexです！")
+    return render(request, 'index.html')
 
