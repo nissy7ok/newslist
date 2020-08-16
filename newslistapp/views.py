a78bd1ecdf7737b3a396c1df1a839b28a815d98e
nissy7ok <nishimrua.n.ku@gmail.com>
2020-08-16 17:39:34 +0900

    create header
    create header with bootstrap

--- newslistapp/views.py
+++ newslistapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Article
+
 def index(request):
     # return HttpResponse("Indexです！")
-    return render(request, 'index.html')
+    articles = Article.objects.all()
+    return render(request, 'index.html', {'articles': articles})
 
