b3c64a687627c20bf73e4d898e274ee204966342
nissy7ok <nishimrua.n.ku@gmail.com>
2020-08-18 22:47:04 +0900

    add scraping target
    add 2nd target 'AVILEN AI Trend'
    all article store list & sorted by date
    abolition favicon scrape because of unknown url & considering copyright

--- newslistapp/views.py
+++ newslistapp/views.py
@@ -11,26 +11,48 @@ from bs4 import BeautifulSoup
 from datetime import datetime as dt
 
 def index(request):
-    articles = Article.objects.all()
-    target = "新R25"
-    url = "https://r25.jp/latest"
-    url_pattern = "https?://[^/]+/"
-    m = re.match(url_pattern, url)
-    favicon = m.group() + "favicon.ico"
+    articles = Article.objects.all()    # 保存用DB、まだ使わない
+    
+    targets = ['新R25', 'AVILEN AI Trend']
+    urls = {
+        targets[0]: 'https://r25.jp/latest',
+        targets[1]: 'https://ai-trend.jp/'
+    }
+
+    # 新R25
+    url = 'https://r25.jp/latest'
     res = requests.get(url)
     soup = BeautifulSoup(res.text, "html.parser")
     elems = soup.find_all(href=re.compile("/article/"))
 
     news_list = []
     for elem in elems:
-        title = elem.h3.string
-        date = elem.time.string
+        target = '新R25'
+        icon ='新'
+        title = elem.h3.text
+        date = elem.time.text.strip()
         date = dt.strptime(date, '%Y.%m.%d').date()
         path = elem.attrs["href"]
         url = "https://r25.jp" + path
-        news_list.append([title, date, url])
+        news_list.append([target, icon, title, date, url])
 
-    return render(request, 'index.html', {'target': target, 'favicon': favicon, 'news_list': news_list, 'articles': articles})
+    # AVILEN AI Trend
+    url2 = 'https://ai-trend.jp/'
+    res2 = requests.get(url2)
+    soup2 = BeautifulSoup(res2.text, "html.parser")
+    elems2 = soup2.find_all(class_='article__content')
+
+    for elem in elems2:
+        target = 'AVILEN AI Trend'
+        icon = 'A'
+        title = elem.h3.text.strip()
+        date = elem.p.string
+        date = dt.strptime(date, '%Y.%m.%d %a').date()
+        url = elem.a.attrs["href"]
+        news_list.append([target, icon, title, date, url])
+
+    news_list = sorted(news_list, key=lambda x: x[3], reverse=True)
+    return render(request, 'index.html', {'news_list': news_list, 'articles': articles})
 
 def mypage(request):
     articles = Article.objects.all()
