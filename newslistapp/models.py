8e4714eeabbf71cb6196d5e2f2f4eb946df5fbc8
nissy7ok <nishimrua.n.ku@gmail.com>
2020-08-22 21:46:46 +0900

    delete unique from Article model

--- newslistapp/models.py
+++ newslistapp/models.py
@@ -8,12 +8,11 @@ class Article(models.Model):
     created_at = models.DateField(auto_now_add=True)
     title = models.CharField(
         max_length=100, 
-        unique=True, 
         error_messages={
             'unique': ("This article already exists.")
             },
     )
-    url = models.URLField(max_length=200, unique=True)
+    url = models.URLField(max_length=200)
     name = models.CharField(max_length=100)
     user = models.ForeignKey('users.User', verbose_name='ユーザー名', on_delete=models.CASCADE)
 
