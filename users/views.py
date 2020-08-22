d2d11626ba9b9d0901d9a72f54661dd3a9e8858b
nissy7ok <nishimrua.n.ku@gmail.com>
2020-08-22 21:53:05 +0900

    add alert when signup

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 from .forms import SignUpForm
 from datetime import date
+from django.contrib import messages
 
 User = get_user_model()
 
@@ -16,7 +17,9 @@ def signup(request):
         if signup_form.is_valid():
             user = signup_form.save()
             auth_login(request, user)
+            messages.success(request, '登録内容を保存しました')
             return redirect('newslistapp:index')
     else:
         signup_form = SignUpForm()
     return render(request, 'signup.html', {'signup_form': signup_form})
+    messages.error(request, '登録内容の保存に失敗しました')
