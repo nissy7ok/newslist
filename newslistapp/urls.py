cb02d8825a05d5d20472b771714506767c2026b9
nissy7ok <nishimrua.n.ku@gmail.com>
2020-08-27 17:58:38 +0900

    delete commentout

--- newslistapp/urls.py
+++ newslistapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-from .views.views import mypage, StockNews, delete_stock, exec_ajax
+from .views.views import mypage, StockNews, delete_stock
 from .views.index import index
 
 app_name='newslistapp'
@@ -9,6 +9,4 @@ urlpatterns = [
     path('mypage/', mypage, name='mypage'),
     path('stock/', StockNews.as_view(), name='stock'),
     path('delete_stock/<int:pk>/', delete_stock, name='delete_stock'),
-    # Ajax処理
-    path("exec/", exec_ajax, name='exec'),
 ]
--- newslistapp/views/views.py
+++ newslistapp/views/views.py
@@ -3,14 +3,13 @@ from django.http import HttpResponse
 from django.views.generic import CreateView
 from django.views.decorators.http import require_POST
 from django.urls import reverse_lazy
-from django.contrib import messages
-# from django.contrib.messages.views import SuccessMessageMixin
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth.models import User
 from ..models import Article
-# import logging
+
 from django.http.response import JsonResponse
 
 
@@ -24,32 +23,10 @@ class StockNews(CreateView, LoginRequiredMixin):
     model = Article
     fields = ('user', 'title', 'name', 'url', 'icon')
     success_url = reverse_lazy('newslistapp:index')
-    # def form_valid(self, form):
-    #     messages.success(self.request, "記事を保存しました")
-    #     return super().form_valid(form)
-    # def form_invalid(self, form):
-    #     messages.warning(self.request, "記事は既に保存されています")
-    #     return redirect('newslistapp:index')
 
 @login_required
 @require_POST
 def delete_stock(request, pk):
-    # title = Article.objects.values_list('title', flat=True).get(pk=pk)
-    # for stock in Article.objects.filter(title=title).filter(user=request.user):
-    #     stock.delete()
     stock = get_object_or_404(Article, pk=pk)
     stock.delete()
     return redirect('newslistapp:mypage')
-
-# Ajax処理
-def exec_ajax(request):
-    if request.method == 'POST':  # POSTの処理
-        data1 = request.POST.get("user")  # POSTで渡された値
-        data2 = request.POST.get("title")  # POSTで渡された値
-        data3 = request.POST.get("name")  # POSTで渡された値
-        data4 = request.POST.get("url")  # POSTで渡された値
-        data5 = request.POST.get("icon")  # POSTで渡された値
-        # article = Article(user = data1, title = data2, url = data3, name = data4, icon = data5, created_at = dt.now)
-        # article.save()
-        # logger.debug(data1 + data2 + data3 + data4 + data5)
-        return HttpResponse(data2)
