dba77149909c449879161b63e9ba98162e3a5780
nissy7ok <nishimrua.n.ku@gmail.com>
2020-08-30 16:23:48 +0900

    create easy test-code
    to learning test-code

--- newslistapp/tests.py
+++ newslistapp/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Article
 
-# Create your tests here.
+import sys
+sys.path.append('../')
+from users.models import User
+
+class TestIndex(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass123')
+        self.client = Client()
+        self.client.login(username='testuser', password='testpass123')
+
+    def test_index_too_title_has_100char(self):
+        """タイトル文字数が上限
+        """
+        Article.objects.create(title='1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890', user_id=1)
+        self.assertEqual(1, len(Article.objects.all()))
+
+    def test_index_too_title_has_100char(self):
+        """重複した記事の保存
+        """
+        try:
+            Article.objects.create(title='1234567890', user_id=1)
+            Article.objects.create(title='1234567890', user_id=1)
+        except Exception:
+            print("重複した保存がリクエストされました")
